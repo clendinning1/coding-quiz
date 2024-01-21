415e430ec16467ff3de7b9e5022cb3189c23ea35
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-01-21 16:00:33 -0600

    wrote out js to-dos

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// TO DO!!
+// 1. separate pages so only one appears at a time (only sections, leave header
+//    intact)
+// 2. set up links for:
+//      a. high score header
+//      b. start quiz button
+//      c. every button on the quiz pages
+//      d. go back button
+// 3. set up timer:
+//      a. should start on the start quiz button and end after clicking
+//         a button on page 5
+//      b. should doc 10 secs for every wrong answer (set booleans for each
+//         quiz button?)
+//      c. if the timer runs out you're booted to the final page
+//      d. remaining time is the score
+// 4. set up scorekeeping:
+//      a. grab the initials and pair them with their score
+//      b. keep a running list in local storage
+//      c. the high score page should display them in a numbered list from
+//         highest to lowest
+//      d. the clear high scores button should clear the cache or whatev
+// 5. set up messages:
+//      a. a correct!/wrong! message should pop up every time a quiz button is
+//         clicked
+//      b. display for a second or two, and then fade until the next click
+//      c. what if you click through really fast? make sure it deletes the
+//         prev message before displaying another.
+
