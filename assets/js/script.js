45409f4f73a50f6f9e3a493b3d329da908238dab
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-01-21 20:55:09 -0600

    each page is set to display:none until the correct buttons are clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,7 @@
 // TO DO!!
-// 1. separate pages so only one appears at a time (only sections, leave header
-//    intact)
 // 2. set up links for:
 //      a. high score header
 //      b. start quiz button
-//      c. every button on the quiz pages
 //      d. go back button
 // 3. set up timer:
 //      a. should start on the start quiz button and end after clicking
@@ -26,3 +23,124 @@
 //      c. what if you click through really fast? make sure it deletes the
 //         prev message before displaying another.
 
+
+// VARRIES !!
+// DOM locations
+var page0 = document.getElementById("page0");
+var page1 = document.getElementById("page1");
+var page2 = document.getElementById("page2");
+var page3 = document.getElementById("page3");
+var page4 = document.getElementById("page4");
+var page5 = document.getElementById("page5");
+
+var pagef = document.getElementById("pagef"); // final page
+var pagehs = document.getElementById("pagehs"); // hs page
+var correct = document.getElementById("correct"); // content in the footer (correct/incorrect msg)
+
+// button DOM locations
+var start = document.getElementById("start");
+var p1Btns = document.getElementById("p1btns");
+var p2Btns = document.getElementById("p2btns");
+var p3Btns = document.getElementById("p3btns");
+var p4Btns = document.getElementById("p4btns");
+var p5Btns = document.getElementById("p5btns");
+var submit = document.getElementById("submitbtn");
+
+
+
+
+
+// APPEARIFYING SECTIONS
+
+// button functionality
+start.addEventListener("click", page1Start);
+p1Btns.addEventListener("click", page2Switch);
+p2Btns.addEventListener("click", page3Switch);
+p3Btns.addEventListener("click", page4Switch);
+p4Btns.addEventListener("click", page5Switch);
+p5Btns.addEventListener("click", pagefSwitch);
+submit.addEventListener("click", pagehsSwitch);
+
+// test
+// submit.addEventListener("click", function(){ console.log("workang??"); });
+
+
+
+// functions for the buttons
+
+function page1Start () {
+    // removes page 0, displays page 1
+    document.getElementById("page0").style.display = "none";
+    document.getElementById("page1").style.display = "flex";
+}
+
+function page2Switch () {
+    // removes page 1, displays page 2
+    document.getElementById("page1").style.display = "none";
+    document.getElementById("page2").style.display = "flex";
+}
+
+function page3Switch () {
+    // removes page 2, displays page 3
+    document.getElementById("page2").style.display = "none";
+    document.getElementById("page3").style.display = "flex";
+}
+
+function page4Switch () {
+    // removes page 3, displays page 4
+    document.getElementById("page3").style.display = "none";
+    document.getElementById("page4").style.display = "flex";
+}
+
+function page5Switch () {
+    // removes page 4, displays page 5
+    document.getElementById("page4").style.display = "none";
+    document.getElementById("page5").style.display = "flex";
+}
+
+function pagefSwitch () {
+    // removes page 5, displays final page
+    document.getElementById("page5").style.display = "none";
+    document.getElementById("pagef").style.display = "flex";
+}
+
+function pagehsSwitch () {
+    // removes final page, displays hs page
+    document.getElementById("pagef").style.display = "none";
+    document.getElementById("pagehs").style.display = "flex";
+}
+
+
+
+
+// TIMER
+var timer = document.getElementById('timer');
+
+// Timer that counts down from 75
+function countdown() {
+    var timeLeft = 75;
+
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+        // As long as the `timeLeft` is greater than 1
+        if (timeLeft > 1) {
+            // Set the `textContent` of `timer` to show the remaining seconds
+            timer.textContent = "Time: " + timeLeft;
+            // Decrement `timeLeft` by 1
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+            timer.textContent = "Time: " + timeLeft;
+            timeLeft--;
+        } else {
+            // Once `timeLeft` gets to 0, set `timer` to an empty string
+            timer.textContent = '';
+            // Use `clearInterval()` to stop the timer
+            clearInterval(timeInterval);
+            // Call the `displayMessage()` function
+            displayMessage();
+        }
+    }, 1000);
+}
+
+countdown();
